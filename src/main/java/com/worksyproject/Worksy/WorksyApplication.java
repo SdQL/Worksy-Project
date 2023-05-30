80b85ba728908a354d42f82783553f2d587c01ea
SamyDc <sadavid2007@gmail.com>
2023-05-29 19:28:57 -0500

    Worksy: Frontend structure

---
+++ src/main/java/com/worksyproject/Worksy/WorksyApplication.java
@@ -0,0 +1,13 @@
+package com.worksyproject.Worksy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorksyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorksyApplication.class, args);
+	}
+
+}
